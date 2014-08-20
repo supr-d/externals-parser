3afd3cebe70d6876b940a1b3a94005df8dd9c7de
Дмитрий Виноградов <vinogradov@starline.ru>
2014-08-20 15:05:44 +0400

    optimized parsing with exceptions

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
-#!/Users/Dima/py/VirtualEnv/compressor-parser/bin/python -tt
+#!/usr/bin/python3 -tt
 # -*- coding: utf-8 -*-
 
 from HTMLParser import HTMLParser
 from argparse import ArgumentParser
+import re
 
 
 class HTMLExternalsParser(HTMLParser):
@@ -11,17 +12,48 @@ class HTMLExternalsParser(HTMLParser):
         'js': []
     }
 
+    exception_links = [
+        '/favicon.ico',
+        '/css/map.min.css',
+        '/js/map.min.js',
+        'https://enterprise.api-maps.yandex.ru/2.0.37/'
+    ]
+
+    exception_types = [
+        ('type', 'image/x-icon'),
+        ('type', 'text/html')
+    ]
+
+    tags_to_scan = [
+        'script',
+        'link'
+    ]
+
     @staticmethod
     def parse_link(link):
         return link.split('?')[0]
 
+    @staticmethod
+    def remove_root(link, folder):
+        return re.sub(folder, '', link)
+
+    def check_tag_types(self, tag):
+        for val in self.exception_types:
+            if val in tag:
+                return False
+
+        return True
+
     def handle_starttag(self, tag, attrs):
-        if tag == 'script' or tag == 'link':
+        if tag in self.tags_to_scan and attrs and self.check_tag_types(attrs):
             for attr in attrs:
-                if attr[0] == 'href':
-                    self.files['css'].append(self.parse_link(attr[1]))
-                elif attr[0] == 'src':
-                    self.files['js'].append(self.parse_link(attr[1]))
+                link = self.parse_link(attr[1])
+
+                if link not in self.exception_links:
+                    if attr[0] == 'href':
+                        self.files['css'].append(self.remove_root(link, '/css/'))
+                    elif attr[0] == 'src':
+                        self.files['js'].append(self.remove_root(link, '/js/'))
 
 
 def get_args():
@@ -44,6 +76,18 @@ if __name__ == '__main__':
         parser = HTMLExternalsParser()
         parser.feed(html_file)
 
-        print(parser.files)
+        css_files = []
+        js_files = []
+
+        for css in parser.files['css']:
+            css_files.append(css)
+
+        print(' '.join(css_files))
+        print()
+
+        for js in parser.files['js']:
+            js_files.append(js)
+
+        print(' '.join(js_files))
     except Exception as e:
         print(e)
